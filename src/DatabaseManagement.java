a29f6547afc8952fcd876d17c89bac69e8d3920f
binwon-song <binwon236@gmail.com>
2023-10-26 23:14:46 +0900

    add DB Func(add, select, delete)

---
+++ src/DatabaseManagement.java
@@ -0,0 +1,58 @@
+import java.sql.*;
+
+public class DatabaseManagement {
+    private static Connection connection;
+
+    public static void DBInit() throws SQLException {
+        try{
+            String url = "jdbc:mysql://192.168.31.133:4567/madang";
+            String userName = "binwon";
+            String password = "1234";
+            connection = DriverManager.getConnection(url, userName, password);
+        }catch(Exception e){System.out.println(e);}
+
+    }
+
+    public static void DBInsert(Integer id, String name,String publisher, Integer price) throws SQLException {
+        String sql = "INSERT INTO Book (bookid, bookname,publisher,price) VALUES (?, ?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+        preparedStatement.setString(2, name);
+        preparedStatement.setString(3, publisher);
+        preparedStatement.setInt(4, price);
+
+
+        int rowsAffected = preparedStatement.executeUpdate();
+
+        if (rowsAffected > 0) {
+            System.out.println("Data inserted successfully.");
+        } else {
+            System.out.println("Data insertion failed.");
+        }
+    }
+
+    public static void DBSelect() throws SQLException {
+        String sql = "SELECT * FROM Book";
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+
+        while (rs.next()) {
+            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+        }
+    }
+    public static void DBDelete(int bookId) throws SQLException {
+        String sql = "DELETE FROM Book WHERE bookid = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, bookId);
+
+        int rowsAffected = preparedStatement.executeUpdate();
+
+        if (rowsAffected > 0) {
+            System.out.println("Data deleted successfully.");
+        } else {
+            System.out.println("Data deletion failed. (Book with ID " + bookId + " not found)");
+        }
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
-import java.sql.*;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://192.168.31.133:3308/madang";
-        String userName = "binwon";
-        String password = "1234";
-
-        Connection connection = DriverManager.getConnection(url, userName, password);
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from Book");
-
-        while(resultSet.next())
-            System.out.println(resultSet.getInt(1)+"  "+resultSet.getString(2)+resultSet.getString(3));
-        connection.close();
+        DatabaseManagement.DBInit();
+        DatabaseManagement.DBInsert(23,"testBook","testPub",1234);
+        DatabaseManagement.DBSelect();
+        DatabaseManagement.DBDelete(23);
+        DatabaseManagement.DBSelect();
     }
 }
